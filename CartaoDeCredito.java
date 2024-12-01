e182061fe54f5575496899726c3c2e4da47048d8
Isabella Cardoso <105462290+imcardosoo@users.noreply.github.com>
2024-12-01 03:52:13 -0300

    Atualizar o CartaoDeCredito.java
    Alteração na classe CartaoDeCredito:adição getters/setters

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -13,18 +13,58 @@ public class CartaoDeCredito {
         this.saldo = saldo;
     }
 
+        public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getCpfTitular() {
+        return cpfTitular;
+    }
+
+    public void setCpfTitular(String cpfTitular) {
+        this.cpfTitular = cpfTitular;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     public String consultarSaldo() {
-        return String.format("Saldo disponível: R$ %.2f", saldo);
+        return String.format("Saldo disponível: R$ %.2f", getSaldo());
     }
 
     public String consultarLimite() {
-        return String.format("Limite total: R$ %.2f", limite);
+        return String.format("Limite total: R$ %.2f", getLimite());
     }
 
     public String realizarTransacao(double valor) {
         if (valor <= 0) return "Valor inválido para transação.";
-        if (valor > (limite - saldo)) return "Transação negada: limite insuficiente.";
-        saldo += valor;
-        return String.format("Transação de R$ %.2f realizada com sucesso! Saldo atual: R$ %.2f", valor, saldo);
+        if (valor > (getLimite() - getSaldo())) return "Transação negada: limite insuficiente.";
+        setSaldo(getSaldo() + valor);
+        return String.format("Transação de R$ %.2f realizada com sucesso! Saldo atual: R$ %.2f", valor, getSaldo());
     }
 }
