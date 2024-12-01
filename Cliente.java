f207b4f01b698174f23c6b0325a642139588f373
Isabella Cardoso <105462290+imcardosoo@users.noreply.github.com>
2024-12-01 04:12:02 -0300

    Atualizar o Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,57 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String cnpj;
+    private String email;
+    private String telefone;
+
+    // Construtor
+    public Cliente(String nome, String cpf, String cnpj, String email, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cnpj = cnpj;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // Métodos getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
