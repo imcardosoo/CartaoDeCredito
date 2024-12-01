ac1883cf8a04cf771dbc3d9804ba41d8c8907e2b
Isabella Cardoso <105462290+imcardosoo@users.noreply.github.com>
2024-12-01 04:44:59 -0300

    Atualizar o Transacao.java

--- Transacao.java
+++ Transacao.java
@@ -2,36 +2,17 @@ import java.util.Date;
 
 public class Transacao {
     private Date data;
-    private float valor;
+    private double valor;
     private String descricao;
 
-    public Transacao(Date data, float valor, String descricao) {
+    public Transacao(Date data, double valor, String descricao) {
         this.data = data;
         this.valor = valor;
         this.descricao = descricao;
     }
 
-    public Date getData() {
-        return data;
-    }
-
-    public void setData(Date data) {
-        this.data = data;
-    }
-
-    public float getValor() {
-        return valor;
-    }
-
-    public void setValor(float valor) {
-        this.valor = valor;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public String toString() {
+        return String.format("Data: %s | Valor: R$ %.2f | Descrição: %s", data, valor, descricao);
     }
 }
